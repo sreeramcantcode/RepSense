ac3267085b6d0f22a3649632ec1edb4b39e7a878
Eighth-Horcruxxx <sreeramsunthar@gmail.com>
2026-04-15 13:21:33 +0530

    Firebase integration

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@
 
 import { useState , useEffect } from "react"
 import { db } from "@/lib/firebase"
-import { ref, onValue, runTransaction } from "firebase/database";
+import { ref, onValue, runTransaction , get,set } from "firebase/database";
+
 
 function App (){
 
   const [count , setcount] = useState(0)
   const [ingym , setingym] = useState(false)
+  const [email , setemail] = useState("");
 
   useEffect(()=>{
     const countref = ref(db,"gym/count");
@@ -23,13 +25,55 @@ function App (){
 
     }, []);
 
-    const handleclick = ()=>{
-      const countref = ref(db , "gym/count")
-      runTransaction(countref , (current)=>{
-        return (current || 0)+1;
-    })}
+    const handleclick = async ()=>{
+      
+      if(!email || !email.includes("@") ){
+        alert("Enter valid email mah nigga")
+        setemail("")}
+      else if(!(email.endsWith("@vitapstudent.ac.in"))){
+         setemail("")
+         
+        alert("Use clg mail only")
+        
+
+      }
+       
+      else{
+         const countref = ref(db , "gym/count")
+
+        await runTransaction(countref , (current)=>{
+          
+          return (current || 0)+1;
+          
+
+    }
+
+  );
+    const userref = ref(db , `users/${email.replace(/\./g, "_")}`);
+     const snapshot = await get(userref)
+     const today = new Date().toISOString().split("T")[0] // Returns date and ignores time
+
+     if (snapshot.exists()) {
+  const data = snapshot.val();
+
+  await set(userref, {
+    lastVisit: today,
+    totalVisits: (data.totalVisits || 0) + 1,
+
+  });
+} 
+else {
+  await set(userref, {
+    lastVisit: today,
+    totalVisits: 1,
+  });
+}
 
 
+    setingym(true)  }
+       
+}
+  
   const handleclick2 = ()=>{
     const countref = ref(db , "gym/count")
     runTransaction(countref , (current)=>{
@@ -38,6 +82,8 @@ function App (){
 
     
     }
+
+   
 
 
   return(
@@ -51,10 +97,14 @@ function App (){
       <input
         type="email"
         placeholder="Enter college email"
+        value={email}
+        onChange={(e)=> setemail(e.target.value)}
         className="border p-2 rounded w-64 bg-white text-black"
       />
+
+      
       {!ingym ? (
-             <button onClick={() => {handleclick(); setingym(true);}} className="bg-black text-white px-8 py-2 rounded">
+             <button onClick={() => {handleclick();}} className="bg-black text-white px-8 py-2 rounded">
         Enter Gym
       </button>
         ) : (
